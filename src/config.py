0bb523b2c98db8d1641b97fddf36adcc3dac2319
Alexander Klimov <alexandrklimov04@gmail.com>
2024-12-01 21:39:22 +0700

    refactor config

--- src/config.py
+++ src/config.py
@@ -5,51 +5,45 @@ from dotenv import find_dotenv, load_dotenv
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from collections.abc import AsyncGenerator
 
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine, AsyncEngine
 
 
 load_dotenv(find_dotenv(".env"))
 
+class DB(BaseSettings):
+    POSTGRES_HOST: str = "localhost"
+    POSTGRES_PORT: int = 5432
+    POSTGRES_USER: str = "postgres"
+    POSTGRES_PASSWORD: str = "1"
+    POSTGRES_DB: str = "postgres"
+    SCHEMA_CONTENT: str = "public"
 
-class Settings(BaseSettings):
-    class DB(BaseSettings):
-        POSTGRES_HOST: str = "localhost"
-        POSTGRES_PORT: int = 5432
-        POSTGRES_USER: str = "postgres"
-        POSTGRES_PASSWORD: str = "1"
-        POSTGRES_DB: str = "postgres"
-        SCHEMA_CONTENT: str = "public"
-
-        @property
-        def POSTGRES_DSN(self) -> str:
-            return (
-                f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}"
-                f"@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
-            )
-
-        async def get_async_session(self) -> AsyncGenerator[AsyncSession, None]:
-            async with self.async_session_maker() as session:
-                yield session
-
+    @property
+    def POSTGRES_DSN(self) -> str:
+        return (
+            f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}"
+            f"@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+        )
 
 
     class Logging:
         LOG_LEVEL: str = "INFO"
         LOG_FORMAT: str = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 
-    model_config = SettingsConfigDict(case_sensitive=True)
+        def setup_logging(self):
+            logging.basicConfig(level=self.Logging.LOG_LEVEL, format=self.Logging.LOG_FORMAT)
 
-    def setup_logging(self):
-        logging.basicConfig(level=self.Logging.LOG_LEVEL, format=self.Logging.LOG_FORMAT)
+
+class AccessToken(BaseSettings):
+    LIFETIME_SECONDS: int = 3600
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(case_sensitive=True)
 
     LOGGING: ClassVar[Logging] = Logging()
     DATABASES: ClassVar[DB] = DB()
-
-    LIFETIME_TOKEN_SECONDS: int = 3600
-
-    engine: AsyncEngine = create_async_engine(DATABASES.POSTGRES_DSN)
-    async_session_maker: async_sessionmaker = async_sessionmaker(engine, expire_on_commit=False)
+    ACCEES_TOKEN: ClassVar[AccessToken] = AccessToken()
 
 
 settings = Settings()
-settings.setup_logging()
+settings.LOGGING.setup_logging()
