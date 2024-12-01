6c56e2fc66e18cb3c34e102060a5436ca9833c72
Alexander Klimov <alexandrklimov04@gmail.com>
2024-12-01 21:32:42 +0700

    refactor strategy

--- src/dependencies/strategy.py
+++ src/dependencies/strategy.py
@@ -1,13 +1,28 @@
-import uuid
+from typing import (
+    TYPE_CHECKING,
+    Annotated,
+)
 
 from fastapi import Depends
-from fastapi_users.authentication.strategy.db import AccessTokenDatabase, DatabaseStrategy
+from fastapi_users.authentication.strategy.db import (
+    DatabaseStrategy,
+)
 
 from config import settings
-from models import AccessToken
+from .access_tokens import get_access_tokens_db
+
+if TYPE_CHECKING:
+    from models import AccessToken
+    from fastapi_users.authentication.strategy.db import AccessTokenDatabase
 
 
 def get_database_strategy(
-    access_token_db: AccessTokenDatabase[AccessToken] = Depends(get_access_token_db),
+    access_tokens_db: Annotated[
+        "AccessTokenDatabase[AccessToken]",
+        Depends(get_access_tokens_db),
+    ],
 ) -> DatabaseStrategy:
-    return DatabaseStrategy(access_token_db, lifetime_seconds=settings.LIFETIME_TOKEN_SECONDS)
+    return DatabaseStrategy(
+        database=access_tokens_db,
+        lifetime_seconds=settings.LIFETIME_TOKEN_SECONDS,
+    )
